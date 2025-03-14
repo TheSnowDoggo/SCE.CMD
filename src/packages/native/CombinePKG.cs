c95e429fafe13a6fc6448519ebb616b87506bb3e
Luna Bl3p <redstonewolfer@gmail.com>
2025-03-14 07:20:17 +0000

    cc

---
+++ src/packages/native/CombinePKG.cs
@@ -0,0 +1,58 @@
+﻿namespace SCE
+{
+    internal class CombinePKG : Package
+    {
+        private readonly HashSet<string> _combines = new();
+
+        public CombinePKG()
+        {
+            Name = "Combine";
+            Commands = new()
+            {
+                { "combine", new(CombineCMD) { MinArgs = 2, MaxArgs = -1,
+                    Description = "Combines multiple commands into one." } },
+
+                { "combdel", new(DeleteCMD) { MinArgs = 1, MaxArgs = -1,
+                    Description = "Deletes the given combine commands." } },
+            };
+        }
+
+        private void DeleteCMD(string[] args, Cmd.Callback cb)
+        {
+            if (args[0] == "*")
+            {
+                if (_combines.Count == 0)
+                    cb.Launcher.FeedbackLine("No commands to remove.");
+                else
+                {
+                    foreach (var comb in _combines)
+                        Commands.Remove(comb);
+                    cb.Launcher.FeedbackLine($"Sucessfully removed {_combines.Count} command(s).");
+                    _combines.Clear();
+                }
+            }
+            else
+            {
+                foreach (var arg in args)
+                {
+                    if (!_combines.Contains(arg))
+                        throw new CmdException("Combine", $"Unknown command \'{arg}\'.");
+                    Commands.Remove(arg);
+                    _combines.Remove(arg);
+                }
+                cb.Launcher.FeedbackLine($"Sucessfully removed {args.Length} command(s).");
+            }
+        }
+
+        private void CombineCMD(string[] args, Cmd.Callback cb)
+        {
+            if (cb.Launcher.CommandExists(args[0]))
+                throw new CmdException("Combine", $"Command \'{args[0]}\' already exists.");
+            var trim = ArrUtils.TrimFirst(args);
+            Commands.Add(args[0], new(_ => cb.Launcher.ExecuteEveryCommand(trim)) { 
+                Description = $"Combined command of {trim.Length} line(s)." });
+            _combines.Add(args[0]);
+            cb.Launcher.FeedbackLine($"Sucessfully made new command \'{args[0]}\'.");
+        }
+    }
+}
