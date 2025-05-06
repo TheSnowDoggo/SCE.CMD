08cc52b8c4932dba75238e52d9dfcebc86e57788
Luna Bl3p <redstonewolfer@gmail.com>
2025-05-06 20:42:10 +0100

    cc

--- src/Main.cs
+++ src/Main.cs
@@ -6,7 +6,7 @@
         {        
             CmdLauncher launcher = new()
             {
-                Name = "- SCE Launcher v0.1.1 -",
+                Name = "- SCE Launcher v0.2.1 -",
                 Packages = new()
                 {
                     new NativePKG(),
