298d74ba0bada5840635e6f2bcf627290ce5bbf1
Luna Bl3p <redstonewolfer@gmail.com>
2025-05-08 15:18:21 +0100

    cc

--- src/processor/DefinePRP.cs
+++ src/processor/DefinePRP.cs
@@ -6,6 +6,7 @@ namespace SCE
     {
         public const string IGNORE = "@IGNORE";
         public const string END_IGNORE = "@ENDIGNORE";
+        public const string ASL = "@ASL";
 
         public DefinePRP(int priority)
             : base(priority)
@@ -30,6 +31,7 @@ namespace SCE
         {
             StringBuilder sb = new();
             bool ignore = false;
+            int asl = -1;
             for (int i = 0; i < str.Length; ++i)
             {
                 if (Match(str, IGNORE, i))
@@ -46,7 +48,14 @@ namespace SCE
                     i += END_IGNORE.Length - 1;
                     continue;
                 }
-                if (!ignore)
+                else if (Match(str, ASL, i))
+                {
+                    sb.Append(ASL);
+                    asl = ASL.Length;
+                    i += asl - 1;
+                    continue;
+                }
+                if (!ignore && asl != i)
                 {
                     bool found = false;
                     foreach (var def in Defines)
@@ -76,6 +85,8 @@ namespace SCE
                     i += IGNORE.Length - 1;
                 else if (Match(str, END_IGNORE, i))
                     i += END_IGNORE.Length - 1;
+                else if (Match(str, ASL, i))
+                    i += ASL.Length - 1;
                 else
                     sb.Append(str[i]);
             }
